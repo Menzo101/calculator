0f1d65e74ad9c2a638288acc52db1313d1413d6e
oluwatobi <emmanuelshola045@gmail.com>
2024-11-30 04:36:15 -1100

    first commit

---
+++ javascript/cal.js
@@ -0,0 +1,11 @@
+ //menzo java calc
+  let display = document.getElementById("display");
+ function appendToDisplay(input){
+     display.value += input;
+ }
+ function clearDisplay(){
+      display.value = " ";
+ }
+ function calculate(){
+   display.value = eval(display.value)
+ }
